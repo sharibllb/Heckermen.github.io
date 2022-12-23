65bd4a89e19771af9a9a0a4087d9ef08b20d5d41
Sharib Ansari <112088064+sharibllb@users.noreply.github.com>
2022-12-23 19:40:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ let a = [
     "Match not found",
     "Match found in 423K",
     "Accessing Account detalis",
-    "Hack Successful"
-    "Dont't fear, just close the tab Now"
-    "You are responsible for all this"
-    "Haaaaa Haaaa Haaa"
+    "Hack Successful",
+    "Dont't fear, just close the tab Now",
+    "You are responsible for all this",
+    "Haaaaa Haaaa Haaa",
 ]
-let c = "Alert Close the tab Within Seconds, before the setup is complete;;;;; code:<000:453/588:300.434/>";
+let c = "Alert Close the tab Within  Seconds, before the setup is complete;;; code:<000:453/588:300.434/>";
 
 let b = "To the 'World of Hecking'<br>";
 

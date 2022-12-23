36a8610694e6553ebb7132ec77cd8578fb4ea33c
Sharib Ansari <sharibllb@gmail.com>
2022-12-23 19:18:33 +0530

    Hecked

---
+++ script.js
@@ -0,0 +1,60 @@
+console.log('Exercise 5 Solution');
+
+let a = [
+    "Initializing Hack Tool...",
+    "Searching for device please wait....",
+    "Connecting to server0.023",
+    "Connection failed. Retrying....",
+    "Connection established",
+    "fetched username please wait....",
+    "Trying Brute Force",
+    "213K Password tried....",
+    "Match not found",
+    "384K Password tried...",
+    "Match not found",
+    "Match found in 423K",
+    "Accessing Account detalis",
+    "Hack Successful"
+]
+let c = "Alert Close the tab Within 5 Seconds; code:<000:453/588:300.434/>";
+
+let b = "To the 'World of Hecking'<br>";
+
+
+const secondWarn = async ()=>{
+    setTimeout(() => {
+        // tab.innerHTML = tab.innerHTML + c
+        alert(c)
+    },7000 );
+}
+secondWarn();
+
+const warn =()=>{
+  
+    // alrt.innerHTML = alrt.innerHTML + b
+    setTimeout(() => {
+        alrt.innerHTML = alrt.innerHTML + b
+    }, 2000);
+}
+warn();
+
+const sleep = async (seconds)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve(true)
+        }, seconds*4000);
+    })
+}
+
+const showHack = async (message)=>{
+    await sleep(2)
+    console.log(message);
+    tex.innerHTML = tex.innerHTML + message + "<br>"
+}
+
+(async ()=>{
+    for(let i=0; i<a.length; i++){
+      await showHack(a[i])
+    }
+    
+})()
